9a9f09d8cfe26073ff5f2dcf48c9cd34580ce4c7
William Lindner <wlindner522@gmail.com>
2021-02-06 20:35:43 -0800

    Fix Like Button

--- components/LikeButton.js
+++ components/LikeButton.js
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import useSWR from 'swr'
+import { useState, useEffect } from 'react'
 import Fab from '@material-ui/core/Fab'
 import Badge from '@material-ui/core/Badge'
 import FavoriteIcon from '@material-ui/icons/Favorite'
@@ -8,31 +7,37 @@ const endpoint = '/api/likes'
 
 const LikeButton = () => {
   const [liked, setLiked] = useState(false)
+  const [likeCount, setLikeCount] = useState(null)
 
-  const { data, error, mutate } = useSWR(endpoint)
+  useEffect(() => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then(({ count }) => {
+        setLikeCount(count)
+        console.log(count)
+      })
+  }, [])
 
-  const incrementLike = async () => {
-    const originalCount = data.count || 0
-
+  const incrementLike = () => {
     setLiked(true)
-    data.count = originalCount + 1
-
-    await fetch(endpoint, {
+    setLikeCount(likeCount + 1)
+    fetch(endpoint, {
       body: JSON.stringify({}),
       method: 'POST',
-    }).then((response) => {
-      if (response.status !== 200) {
-        setLiked(false)
-        mutate({ count: originalCount })
-      }
     })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+      })
+      .catch(() => {
+        setLiked(false)
+        setLikeCount(likeCount)
+      })
   }
 
-  if (error) return <div>Error</div>
-  if (!data) return <div>Loading...</div>
   return (
     <Fab aria-label="like" onClick={incrementLike}>
-      <Badge badgeContent={data.count} color="primary">
+      <Badge badgeContent={likeCount} color="primary">
         <FavoriteIcon color={liked ? 'secondary' : 'action'} />
       </Badge>
     </Fab>
