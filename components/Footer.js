5a38c0a9734543e29f6f984bafad105270dc4277
William Lindner <wlindner522@gmail.com>
2022-03-05 21:16:13 -0800

    Improve footer for essay page

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,17 @@ import { Link as ChakraLink, Flex } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Flex padding="20px" zIndex={1} position="fixed" bottom={0} left={0}>
+    <Flex
+      width="100%"
+      maxWidth="700px"
+      padding="20px"
+      position="fixed"
+      bottom={0}
+      left={0}
+      opacity={0.9}
+      backgroundColor="#333"
+      zIndex={1}
+    >
       <ChakraLink
         mr="32px"
         color="white"
